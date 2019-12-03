28205f49be5f914af83d6c6aeb997fcaa0fcf496
CharulAggarwalSP <55969879+CharulAggarwalSP@users.noreply.github.com>
2019-12-03 17:17:07 +0530

    Update order_update.php

--- app/admin/views/_mail/order_update.php
+++ app/admin/views/_mail/order_update.php
@@ -17,6 +17,30 @@ Your order **{order_number}** has been updated to the following status: <br>
 
 The comments for your order are: <br>
 **{status_comment}**
+<!-- BODY -->
+<table class="body-wrap">
+    <tr>
+        <td></td>
+        <td class="container" bgcolor="#FFFFFF">
+            <div class="content">
+                <table>
+                    <tr>
+                        <td>
+                            <h3>Order Update!</h3>
+                            <p>Your order
+                                <strong>{order_number}</strong> has been updated to the following status: </strong>{status_name}</strong>
+                            </p>
+                            <p><strong>The comments for your order are:</strong><br>{status_comment}</p>
+                            <p><a href="{order_view_url}">Click here</a> to view your order progress.</p>
+                        </td>
+                    </tr>
+                </table>
+            </div><!-- /content -->
+        </td>
+        <td></td>
+    </tr>
+</table><!-- /BODY -->
+
 
 @partial('button', ['url' => '{order_view_url}', 'type' => 'primary'])
 View your order progress
