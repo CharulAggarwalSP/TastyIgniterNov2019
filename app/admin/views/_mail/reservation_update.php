fee3eaa1fa1e4c5f5bf639e0d0f4a5d47cb39264
CharulAggarwalSP <55969879+CharulAggarwalSP@users.noreply.github.com>
2019-12-03 17:20:31 +0530

    Update reservation_update.php

--- app/admin/views/_mail/reservation_update.php
+++ app/admin/views/_mail/reservation_update.php
@@ -17,7 +17,29 @@ Your reservation **{reservation_number}** at **{location_name}** has been update
 
 The comments for your reservation are: <br>
 {status_comment}
-
+<!-- BODY -->
+<table class="body-wrap">
+    <tr>
+        <td></td>
+        <td class="container" bgcolor="#FFFFFF">
+            <div class="content">
+                <table>
+                    <tr>
+                        <td>
+                            <h3>Reservation Update!</h3>
+                            <p>Your reservation <strong>{reservation_number}</strong> at
+                                <strong>{location_name}</strong> has been updated to the following status: </strong>{status_name}</strong>
+                            </p>
+                            <p><strong>The comments for your reservation are:</strong><br>{status_comment}</p>
+                            <p><a href="{reservation_view_url}">Click here</a> to view your reservation status.</p>
+                        </td>
+                    </tr>
+                </table>
+            </div><!-- /content -->
+        </td>
+        <td></td>
+    </tr>
+</table><!-- /BODY -->
 @partial('button', ['url' => '{reservation_view_url}', 'type' => 'primary'])
 View your reservation status
 @endpartial
