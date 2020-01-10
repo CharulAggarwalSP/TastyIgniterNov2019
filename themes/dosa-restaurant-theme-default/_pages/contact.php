82af834fa5d172cfb49f9a4d0e9d7ec2485246ee
ramansoft <pankajm.softprodigy@gmail.com>
2020-01-10 20:51:12 +0530

    feedback updates

--- themes/dosa-restaurant-theme-default/_pages/contact.php
+++ themes/dosa-restaurant-theme-default/_pages/contact.php
@@ -13,11 +13,19 @@ permalink: /contact
                     <div class="card-body">
                         <h1 class="h3 card-title"><?= $contactLocation->getName(); ?></h1>
                         <div class="row contact-info mb-2">
-                            <div class="col-1"><i class="fa fa-globe"></i></div>
+                            <div class="col-1"></div>
                             <div class="col"><?= format_address($contactLocation->getAddress()); ?></div>
-                        </div>
+                        </div> 
+                        <div class="row contact-info mb-2">
+                            <div class="col-1"></div>
+                            <div class="col"><a href="mailto:dosarestaurant@gmail.com" target="_top">dosarestaurant@gmail.com</a></div>
+                        </div> 
                         <div class="row contact-info">
                             <div class="col-1"><i class="fa fa-phone"></i></div>
+                            <div class="col">020-6164838</div>
+                        </div>
+                        <div class="row contact-info">
+                            <div class="col-1"><i class="fa fa-mobile"></i></div>
                             <div class="col"><?= $contactLocation->getTelephone(); ?></div>
                         </div>
                     </div>
