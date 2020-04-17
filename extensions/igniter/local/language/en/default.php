72e8e65f3858d01eac83a41c7c1ecf54eaf4d412
pankajkmehta <40817452+pankajkmehta@users.noreply.github.com>
2020-04-17 18:18:56 +0530

    Update default.php
    Text update

--- extensions/igniter/local/language/en/default.php
+++ extensions/igniter/local/language/en/default.php
@@ -77,7 +77,7 @@ return [
     'label_hide_min_order_amount' => 'Hide Min. Order Amount',
     'label_lang' => 'Language Line',
     'label_status' => 'Status',
-    'label_search_query' => 'Enter your address/postcode to order',
+    'label_search_query' => 'Enter your postcode to order',
     'label_redirect' => 'Page to redirect to when no location is selected',
     'label_menu_page' => 'Menus Page',
     'label_show_local_image' => 'Show Location Thumb',
