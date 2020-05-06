63207212b870c790ab8a69190dd21ad9f4bafd31
pankajkmehta <40817452+pankajkmehta@users.noreply.github.com>
2020-05-06 12:35:45 +0530

    Update footer.php
    Privacy url update

--- themes/dosa-restaurant-theme/_partials/footer.php
+++ themes/dosa-restaurant-theme/_partials/footer.php
@@ -134,7 +134,7 @@
         <div class="row">
             <div class="col p-2">                 
                 <?php echo sprintf(lang('main::lang.site_copyright')).' '. date('Y').' '.setting('site_name'); ?>&nbsp;&nbsp;&nbsp;
-                <a href="<?=assets_url('media/policy/privacy.pdf')?>" target="_blank">Privacy Policy</a>&nbsp;&nbsp;&nbsp;
+                <a href="<?=page_url('pages/privacy')?>" target="_blank">Privacy Policy</a>&nbsp;&nbsp;&nbsp;
                 <a href="<?=assets_url('media/policy/cookie.pdf')?>" target="_blank">Cookies Policy</a>
             </div>
         </div>
